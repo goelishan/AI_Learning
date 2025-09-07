78dd41cf06ae27a11273a48107d4f34ff8cf979f
Ishan Goel <ishan.goel24@hotmail.com>
2025-09-07 15:39:55 +0530

    third push

--- loops_basics.py
+++ loops_basics.py
@@ -31,6 +31,30 @@
 #     print(7,'*',i,'=',7*i)
 
 # print numbers divisible by 3
-for i in range(1,51):
-    if i%3==0:
-        print(i)
+# for i in range(1,51):
+#     if i%3==0:
+#         print(i)
+
+# ------------------------------------------------------------------------------------------------------------
+
+# Better approach for above code snippet
+
+# Sum of numbers - 
+
+# print(sum(range(1,101)))
+
+# Multiplication of table - 7
+
+# for i in range(1,11):
+#     print(f"7 x {i}={7*i}")
+
+# Countdown
+
+# n=8
+# for i in range(n,-1,-1):
+#     print(i)
+
+# Divisible by 3
+
+for i in range(3,51,3):
+    print(i)
