08107f1853345d26a99b63e2cfb7aa4a28b024c7
Ishan Goel <ishan.goel24@hotmail.com>
2025-09-07 15:09:16 +0530

    Add loop practice exercises

---
+++ loops_basics.py
@@ -0,0 +1,36 @@
+
+
+# for i in range(1,11):
+#     if i % 2 == 0:
+#         print(i,'is even')
+#     else:
+#         print(i,'is odd')
+
+# while loops
+
+# count=8
+# while count>0:
+#     print("Countdown - ",count)
+#     count=count-1
+
+
+# for i in range(1,31,2):
+#     if i%2 !=0:
+#         print(i,'is odd')
+
+
+# Sum of Numbers
+
+# sum=0
+# for i in range(1,101):
+#     sum=sum+i
+# print(sum)
+
+# multiplication of table - 7
+# for i in range(1,11):
+#     print(7,'*',i,'=',7*i)
+
+# print numbers divisible by 3
+for i in range(1,51):
+    if i%3==0:
+        print(i)
