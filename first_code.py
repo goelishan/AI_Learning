4fdedd7d4942acece3977c268f9de1b04fa492a8
Ishan Goel <ishan.goel24@hotmail.com>
2025-09-07 14:39:18 +0530

    Day 1: Initial scripts and API test

---
+++ first_code.py
@@ -0,0 +1,14 @@
+# import requests
+
+# print('Hello! AI Engineer')
+
+# response=requests.get("https://api.github.com")
+
+# print('Git response - ',response.status_code)
+
+
+# for i in range(10):
+#     print("Ishan Goel")
+
+text = "Pallindrome"
+print(text[::-1])
