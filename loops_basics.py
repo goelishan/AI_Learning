246af7de510aa7afd13f4a613dd521e7c31ef1a5
Ishan Goel <ishan.goel24@hotmail.com>
2025-09-07 15:43:10 +0530

    fourth push

--- loops_basics.py
+++ loops_basics.py
@@ -1,60 +1,25 @@
+# Even / Odd numbers
+for i in range(1, 11):
+    print(f"{i} is {'even' if i % 2 == 0 else 'odd'}")
 
+# Countdown using while
+count = 8
+while count:
+    print(f"Countdown - {count}")
+    count -= 1
 
-# for i in range(1,11):
-#     if i % 2 == 0:
-#         print(i,'is even')
-#     else:
-#         print(i,'is odd')
-
-# while loops
-
-# count=8
-# while count>0:
-#     print("Countdown - ",count)
-#     count=count-1
-
-
-# for i in range(1,31,2):
-#     if i%2 !=0:
-#         print(i,'is odd')
-
+# Odd numbers (1 to 30)
+for i in range(1, 31, 2):
+    print(f"{i} is odd")
 
 # Sum of Numbers
+total = sum(range(1, 101))
+print(f"Sum of 1 to 100 = {total}")
 
-# sum=0
-# for i in range(1,101):
-#     sum=sum+i
-# print(sum)
+# Multiplication table of 7
+for i in range(1, 11):
+    print(f"7 x {i} = {7*i}")
 
-# multiplication of table - 7
-# for i in range(1,11):
-#     print(7,'*',i,'=',7*i)
-
-# print numbers divisible by 3
-# for i in range(1,51):
-#     if i%3==0:
-#         print(i)
-
-# ------------------------------------------------------------------------------------------------------------
-
-# Better approach for above code snippet
-
-# Sum of numbers - 
-
-# print(sum(range(1,101)))
-
-# Multiplication of table - 7
-
-# for i in range(1,11):
-#     print(f"7 x {i}={7*i}")
-
-# Countdown
-
-# n=8
-# for i in range(n,-1,-1):
-#     print(i)
-
-# Divisible by 3
-
-for i in range(3,51,3):
+# Numbers divisible by 3
+for i in range(3, 51, 3):
     print(i)
